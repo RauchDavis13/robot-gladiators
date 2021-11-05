e304746786b9ee11273816605ad603330cd75eae
KRD13 <rauchdavis13@gmail.com>
2021-11-05 18:08:58 -0400

    added high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -134,6 +134,22 @@ var endGame = function() {
     window.alert("You've lost your robot in battle!");
   }
 
+  //check local storage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  highScore = highScore ||0;
+  
+  // if player has more money than high score, player has a new high score
+  if(playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money);
+  }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
+
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
 
